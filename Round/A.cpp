aab974e6581bfc70da096770f7c84841423306cb
NERDYLIZARD <bouasavanhhop@gmail.com>
2016-10-25 15:00:41 +0530

    commit

--- Round/A.cpp
+++ Round/A.cpp
@@ -1,31 +1,10 @@
-// 23rd OCT 2016
-//codeforces.com/contest/725/problem/A
-// impl: O(n)
 #include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
-char s[200008];
 
 int main()
 {
-    int n; scanf("%d", &n);
-    scanf("%s", s+1);
-    s[0] = '<';
-    s[n+1] = '>';
-
-    long cnt = 0;
-    int i = 1;
-    while (s[i++] != '>')
-        cnt++;
-
-    i = n;
-    while (s[i--] != '<')
-        cnt++;
-
-    printf("%I32d\n", cnt);
-
 
 }
-
-
--- Round/B.cpp
+++ Round/B.cpp
@@ -1,29 +1,10 @@
-// 22nd OCT 2016
-//codeforces.com/contest/725/problem/B
-// impl: O(n)
-    // elegant look-up table.
-
-#include <iostream>
-#include <string>
+#include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
+
 int main()
 {
-                //a, b, c, d, e, f
-    int mapp[] = {4, 5, 6, 3, 2, 1};
-    long long n; char c;
-    cin >> n >> c;
-
-    long long block = (n-1) / 4;
-    long long ans = ((6 + 1 + 6) + 3) * block;
-
-    if (!(n&1))
-        ans += 7;
-
-    ans += mapp[c - 'a'];
-    cout << ans;
-
 
 }
-
